de945ceb899783d02ac1a2841ba5d53dc249b053
nivedita0604 <niveditakumari0604@gmail.com>
2021-07-03 16:00:24 +0530

    intialized & added firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyDk4DCl7x2ozHq5Du-I_byz4JfQxvQicos',
+  authDomain: 'chat-web-app-a6282.firebaseapp.com',
+  projectId: 'chat-web-app-a6282',
+  storageBucket: 'chat-web-app-a6282.appspot.com',
+  messagingSenderId: '651434146898',
+  appId: '1:651434146898:web:5b3a040e7d8921a07b338b',
+};
