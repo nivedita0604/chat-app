235da2869ce407fa7f0f83882507e44a73a01b12
nivedita0604 <niveditakumari0604@gmail.com>
2021-07-05 21:24:15 +0530

    created chat page and homepage

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './styles/main.scss';
 import { Switch } from 'react-router';
 import Signin from './pages/Signin';
 import PrivateRouter from './components/PrivateRouter';
-import Home from './pages/Home';
+import Home from './pages/Home/Index';
 import PublicRouter from './components/PublicRoute';
 import { ProfileProvider } from './context/profile.context';
 
---
+++ src/pages/Home/Index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Switch, Route, useRouteMatch } from 'react-router';
+
+import { Grid, Row, Col } from 'rsuite';
+import Chat from './Chat';
+import { RoomsProvider } from '../../context/rooms.context';
+import Sidebar from '../../components/Sidebar';
+import { useMediaQuery } from '../../misc/custom-hook';
+
+const Home = () => {
+  const isDesktop = useMediaQuery('(min-width: 992px)');
+
+  const { isExact } = useRouteMatch();
+
+  const canRenderSidebar = isDesktop || isExact;
+
+  return (
+    <RoomsProvider>
+      <Grid fluid className="h-100">
+        <Row className="h-100">
+          {canRenderSidebar && (
+            <Col xs={24} md={8} className="h-100">
+              <Sidebar />
+            </Col>
+          )}
+
+          <Switch>
+            <Route exact path="/chat/:chatId">
+              <Col sm={24} md={16} className="h-100">
+                <Chat />
+              </Col>
+            </Route>
+            <Route>
+              {isDesktop && (
+                <Col xs={24} md={16} className="h-100">
+                  <h6 className="text-center mt-page">Please select chat</h6>
+                </Col>
+              )}
+            </Route>
+          </Switch>
+        </Row>
+      </Grid>
+    </RoomsProvider>
+  );
+};
+
+export default Home;
