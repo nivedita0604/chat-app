367285491a1fc268ca626b1cd58bcd82b53d960e
nivedita0604 <niveditakumari0604@gmail.com>
2021-07-09 13:18:09 +0530

    added room drawer

--- src/components/chat-window/top/index.js
+++ src/components/chat-window/top/index.js
@@ -5,6 +5,7 @@ import { ButtonToolbar, Icon } from 'rsuite';
 import { useMediaQuery } from '../../../misc/custom-hook';
 import { useCurrentRoom } from '../../../context/current-room.context';
 import RoomInfoBtnModal from './RoomInfoBtnModal';
+import EditRoomBtnDrawer from './EditRoomBtnDrawer';
 
 const index = () => {
   const name = useCurrentRoom(v => {
@@ -15,7 +16,7 @@ const index = () => {
   const isMobile = useMediaQuery('(max-width: 992px)');
   return (
     <div>
-      <div className="d-flex justify-context-between align-items-center">
+      <div className="d-flex justify-content-between align-items-center">
         <h4 className="text-disappear d-flex align-items-center">
           <Icon
             componentClass={Link}
@@ -30,12 +31,12 @@ const index = () => {
           />
           <span className="text-disappear">{name}</span>
         </h4>
-        <div>
-          <ButtonToolbar className="ws-nowrap">
-            <span>todo</span>
-          </ButtonToolbar>
-        </div>
+
+        <ButtonToolbar className="ws-nowrap">
+          <EditRoomBtnDrawer />
+        </ButtonToolbar>
       </div>
+
       <div className="d-flex justify-content-between align-items-left">
         <span>todo</span>
         <RoomInfoBtnModal />
