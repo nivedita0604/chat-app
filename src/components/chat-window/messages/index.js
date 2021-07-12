bd0328968bb533d840a7a335edfdff4125d65977
nivedita0604 <niveditakumari0604@gmail.com>
2021-07-12 21:05:45 +0530

    Pagination and Control of Scrolled Position

--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -1,39 +1,89 @@
 /* eslint-disable consistent-return */
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import { useParams } from 'react-router';
-import { Alert } from 'rsuite';
-import { auth, database, storage } from '../../../misc/firebase';
-import { groupBy, transformToArrWithId } from '../../../misc/helpers';
+import { Alert, Button } from 'rsuite';
+import { database, auth, storage } from '../../../misc/firebase';
+import { transformToArrWithId, groupBy } from '../../../misc/helpers';
 import MessageItem from './MessageItem';
 
-const Message = () => {
+const PAGE_SIZE = 15;
+const messagesRef = database.ref('/messages');
+
+function shouldScrollToBottom(node, threshold = 30) {
+  const percentage =
+    (100 * node.scrollTop) / (node.scrollHeight - node.clientHeight) || 0;
+
+  return percentage > threshold;
+}
+
+const Messages = () => {
   const { chatId } = useParams();
   const [messages, setMessages] = useState(null);
+  const [limit, setLimit] = useState(PAGE_SIZE);
+  const selfRef = useRef();
 
   const isChatEmpty = messages && messages.length === 0;
   const canShowMessages = messages && messages.length > 0;
 
-  useEffect(() => {
-    const messagesRef = database.ref('/messages');
+  const loadMessages = useCallback(
+    limitToLast => {
+      const node = selfRef.current;
 
-    // llstener to all the items of references chatId
-    messagesRef
-      .orderByChild('roomId')
-      .equalTo(chatId)
-      .on('value', snap => {
-        const data = transformToArrWithId(snap.val());
-        setMessages(data);
+      messagesRef.off();
+
+      messagesRef
+        .orderByChild('roomId')
+        .equalTo(chatId)
+        .limitToLast(limitToLast || PAGE_SIZE)
+        .on('value', snap => {
+          const data = transformToArrWithId(snap.val());
+          setMessages(data);
+
+          if (shouldScrollToBottom(node)) {
+            node.scrollTop = node.scrollHeight;
+          }
+        });
+
+      setLimit(p => {
+        return p + PAGE_SIZE;
       });
+    },
+    [chatId]
+  );
+
+  const onLoadMore = useCallback(() => {
+    const node = selfRef.current;
+    const oldHeight = node.scrollHeight;
+
+    loadMessages(limit);
+
+    setTimeout(() => {
+      const newHeight = node.scrollHeight;
+      node.scrollTop = newHeight - oldHeight;
+    }, 200);
+  }, [loadMessages, limit]);
+
+  useEffect(() => {
+    const node = selfRef.current;
+
+    loadMessages();
+
+    setTimeout(() => {
+      node.scrollTop = node.scrollHeight;
+    }, 200);
+
     return () => {
       messagesRef.off('value');
     };
-  }, [chatId]);
+  }, [loadMessages]);
 
   const handleAdmin = useCallback(
     async uid => {
-      const adminRef = database.ref(`/rooms/${chatId}/admins`);
+      const adminsRef = database.ref(`/rooms/${chatId}/admins`);
+
       let alertMsg;
-      await adminRef.transaction(admins => {
+
+      await adminsRef.transaction(admins => {
         if (admins) {
           if (admins[uid]) {
             admins[uid] = null;
@@ -43,14 +93,14 @@ const Message = () => {
             alertMsg = 'Admin permission granted';
           }
         }
+
         return admins;
       });
+
       Alert.info(alertMsg, 4000);
     },
     [chatId]
   );
-
-  // like handler
 
   const handleLike = useCallback(async msgId => {
     const { uid } = auth.currentUser;
@@ -81,7 +131,6 @@ const Message = () => {
 
     Alert.info(alertMsg, 4000);
   }, []);
-  // delete handler
 
   const handleDelete = useCallback(
     async (msgId, file) => {
@@ -119,9 +168,8 @@ const Message = () => {
         try {
           const fileRef = storage.refFromURL(file.url);
           await fileRef.delete();
-          // file
         } catch (err) {
-          return Alert.error(err.message);
+          Alert.error(err.message);
         }
       }
     },
@@ -161,11 +209,18 @@ const Message = () => {
   };
 
   return (
-    <ul className="msg-list custom-scroll">
-      {isChatEmpty && <li>No message yet...</li>}
+    <ul ref={selfRef} className="msg-list custom-scroll">
+      {messages && messages.length >= PAGE_SIZE && (
+        <li className="text-center mt-2 mb-2">
+          <Button onClick={onLoadMore} color="green">
+            Load more
+          </Button>
+        </li>
+      )}
+      {isChatEmpty && <li>No messages yet</li>}
       {canShowMessages && renderMessages()}
     </ul>
   );
 };
 
-export default Message;
+export default Messages;
